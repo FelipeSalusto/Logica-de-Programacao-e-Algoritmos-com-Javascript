69905eb26af7a783a2015bf2b5dae0b80b2950a7
FelipeSalusto <felipesalusto2.0@gmail.com>
2024-10-06 00:13:35 -0300

    Realização do exercício c do cap04

--- cap04-condicoes/exercicios/js/ex4_c.js
+++ cap04-condicoes/exercicios/js/ex4_c.js
@@ -5,6 +5,38 @@ const resp2 = document.querySelector("#outResp2")
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
+    // Limpar o segundo campo de resposta
+    resp2.innerText = ""
+
+    // Ler o valor digitado no campo de formulário
     const valor = Number(frm.inValor.value)
-    // Continuar a realização do exercício
+
+    // Caso valor insuficiente
+    if (valor < 1.00) {
+        alert("Valor insuficiente (no mínimo, R$ 1.00)")
+        frm.inValor.focus()
+        return
+    }
+
+    // Declaração de variáveis
+    let tempo
+    let troco
+
+    // Cria as condições para verificar tempo e troco
+    if (valor >= 3.00) {
+        tempo = 120
+        troco = valor - 3.00
+    } else if (valor >= 1.75) {
+        tempo = 60
+        troco = valor - 1.75
+    } else {
+        tempo = 30
+        troco = valor - 1.00
+    }
+
+    // Exibe as respostas
+    resp1.innerText = `Tempo: ${tempo} min`
+    if (troco > 0) {
+        resp2.innerText = `Troco: ${troco.toFixed(2)}`
+    }
 })
